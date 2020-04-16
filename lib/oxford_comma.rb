0d259154f064f84a71173119f11c56284fee1ad7
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-16 02:50:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  return array.join(' and ') if list.size < 3
-  list[-1] = "and " + list[-1]
-  list.join(', ')
+  return array.join(' and ') if array.size < 3
+  array[-1] = "and " + array[-1]
+  array.join(', ')
 end
 
 oxford_comma(array)
