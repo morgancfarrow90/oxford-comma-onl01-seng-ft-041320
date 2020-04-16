97385d4611cea9db948e2b7dc0034533bfad17fa
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-16 02:54:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+  case arr.size
+  when 0
+    ""
+  when 1
+    arr.first
+  when 2
+    arr.join(' and ')
+  else
+    [arr[0..-2].join(', '), arr.last].join(', and ')
+  end
 end
